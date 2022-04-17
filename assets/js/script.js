317fcd904ffb2f6c6c76e90d5d1d470eb1cc114d
kierstenv <kierstenvicknair22@gmail.com>
2022-04-17 02:01:19 -0500

    past/present/future color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,33 @@ const Interval = luxon.Interval;
 $('#currentDay').text(DateTime.local().toFormat('DDDD'));
 $('#currentTime').text(DateTime.local().toFormat('tt'));
 
-const startHr = DateTime.local().set({hour: 8, minute: 0});
-const endHr = DateTime.local().set({hour: 18, minute: 0});
-const interval = Interval.fromDateTimes(startHr, endHr);
-
-
 timeBlockHr = 8;
 
-while (timeBlockHr < 19) {
+while (timeBlockHr <= 18) {
   const timeBlock = DateTime.local()
-  .set({hour: timeBlockHr, minute: 0})
-  .toFormat('h a');
+  .set({hour: timeBlockHr, minute: 0});
   
-  const timeBlockHrEl = `<div class="hour">${timeBlock}</div>`;
+  const timeBlockHrEl = `<div class="hour">${timeBlock.toFormat('h a')}</div>`;
   const timeBlockContent = `<textarea class="description" id="${timeBlockHr}"></textarea>`;
   const timeBlockBtn = `<button class="saveBtn" id="${timeBlockHr}"><i>Save</i></button>`;
   const timeBlockRow = `<div class="row">${timeBlockHrEl}${timeBlockContent}${timeBlockBtn}</div>`;
-
-  $('<div>').attr('id', `time-block-` + timeBlockHr)
+  
+  $('<div>').attr('id', 'time-block-' + timeBlockHr)
   .append(timeBlockRow)
   .appendTo('#time-blocks');
+  
   timeBlockHr++;
+
+  if (DateTime.local().hasSame(timeBlock, 'hour')) {
+    $('.description').addClass('present');
+  } else if (DateTime.local() < timeBlock.startOf('hour')) {
+    $('.description').addClass('future');
+  } else {
+    $('.description').addClass('past');
+  }
 }
   
-$(`#${timeBlockId}`).on('click', function() {
+$(`#${timeBlockHr}`).on('click', function() {
   const textAreaId = $(this).attr('id');
   const textAreaText = $(`#${textAreaId}`).val();
   localStorage.setItem(textAreaId, textAreaText);
